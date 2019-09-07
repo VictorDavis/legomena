0e1762ca9c1726c9b09cbccbb7f88a1433f6be55
Victor Davis <vadsql@gmail.com>
2019-09-07 14:10:19 +0000

    exclude dashboard app

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setup(
     author_email="vadsql@gmail.com",
     url="http://legomena.herokuapp.com/",
     packages=["legomena"],
+    package_dir = {'': 'scripts'},
+    packages = find_packages('scripts', exclude=['dashboard']),
     install_requires=["nltk", "numpy", "pandas", "scipy"],
     scripts=["scripts/legomena"],
 )
