6babb49341a6ba56147213a17b2cc840048cd136
victordavis <vadsql@gmail.com>
2019-09-09 22:39:17 -0400

    de-nested unit tests

--- test_legomena.py
+++ test_legomena.py
@@ -6,7 +6,7 @@ from scipy.stats import linregress
 import unittest
 
 # classes to test
-from ..legomena import Corpus, SPGC, HeapsModel, KTransformer, LogModel, InfSeriesModel
+from legomena import Corpus, SPGC, HeapsModel, KTransformer, LogModel, InfSeriesModel
 
 # globals
 GRAPHICS_ON = False
