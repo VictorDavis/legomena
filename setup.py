0ce47f4b55c9b9e4f442cc4f1224d97870fc9316
victordavis <vadsql@gmail.com>
2019-10-15 22:28:13 -0400

    fix: no nested package subfolder

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
-setuptools.setup(
+setup(
     name="legomena",
     version="1.0.0",
     description="Tool for exploring types, tokens, and n-legomena relationships in text.",
     license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
+    py_modules=["legomena"],
     author="Victor Davis",
     author_email="vadsql@gmail.com",
     url="http://legomena.herokuapp.com/",
