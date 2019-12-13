c7c41fb29dfbcffcc483be0b2982dcd58b1996a0
victordavis <vadsql@gmail.com>
2019-12-12 19:54:50 -0500

    test: fix exact values in test

--- test_legomena.py
+++ test_legomena.py
@@ -103,11 +103,13 @@ class LegomenaTest(unittest.TestCase):
         assert sum(corpus.k) == corpus.N
         assert len(corpus.types) == corpus.N
         assert corpus.WFD.equals(corpus.fdist)
+        assert corpus.alpha == 0.9196082282619522
+        assert corpus.gamma == 1.7739943128244318
         assert corpus.as_datarow(7) == (
             8354,
             1820,
-            0.9196082282619522,
-            1.7739943128244318,
+            corpus.alpha,
+            corpus.gamma,
             0,
             1009,
             293,
